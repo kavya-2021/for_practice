7fd574d78d9250a110b2ecbbe0edb562cf32bff3
Lakshmi Kavya <vankadaralakshmikavya@gmail.com>
2022-01-19 18:13:39 +0530

    update on host

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./index');
 
 const connect = require('./configs/db');
 
-app.listen(3888,async()=>{
+app.listen(3838,async()=>{
      await connect();
-    console.log('listening on port 3888');
+    console.log('listening on port 3838');
 })
