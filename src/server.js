4dd2805c556dbae570684f23fffe5618140e6ba6
Lakshmi Kavya <vankadaralakshmikavya@gmail.com>
2022-01-19 13:48:44 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./index');
 
 const connect = require('./configs/db');
 
-app.listen(3344,async()=>{
+app.listen(3346,async()=>{
      await connect();
     console.log('listening on port 3344');
 })
